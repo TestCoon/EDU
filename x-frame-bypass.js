acc2ce28909ef78c8d61727a80f18cdee80e707c
John C. Barr <Farwalker3@icloud.com>
2023-06-28 20:47:50 -0400

    Rewrite

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -12,8 +12,6 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 		this.sandbox = '' + this.sandbox || 'allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation' // all except allow-top-navigation
 	}
 	load (url, options) {
-		if (!url || !url.startsWith('http'))
-			throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`)
 		console.log('X-Frame-Bypass loading:', url)
 		this.srcdoc = `<html>
 <head>
